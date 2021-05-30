58432950a73d55e67c9c95be0dda57d077a68184
Charles Sprayberry <me@cspray.io>
2021-05-30 12:26:21 -0400

    Improve output to order events correctly

--- scripts/github-commits.js
+++ scripts/github-commits.js
@@ -4,16 +4,14 @@ import { readJSON, writeJSON } from 'https://deno.land/x/flat/mod.ts';
 const filename = Deno.args[0];
 const data = await readJSON(filename);
 
-const cleanData = {
-    'PushEvent': [],
-    'CreateEvent': [],
-    'WatchEvent': []
-};
+const cleanData = [];
 
 data.forEach(ghEvent => {
     if (cleanData[ghEvent.type]) {
         const cleanEvent = {};
         cleanEvent.id = ghEvent.id;
+        cleanEvent.type = ghEvent.type;
+        cleanEvent.createdAt = ghEvent.created_at;
         cleanEvent.payload = {};
         cleanEvent.payload.repoName = ghEvent.repo.name;
         cleanEvent.payload.repoUrl = `https://github.com/${ghEvent.repo.name}`;
@@ -22,12 +20,16 @@ data.forEach(ghEvent => {
             cleanEvent.payload.branchUrl =  `${cleanEvent.payload.repoUrl}/tree/${cleanEvent.payload.branch}`;
             cleanEvent.payload.head = ghEvent.payload.head.substring(0, 7);
             cleanEvent.payload.headUrl = `${cleanEvent.payload.repoUrl}/commit/${ghEvent.payload.head}`;
-
+        } else if (ghEvent.type === 'CreateEvent') {
+            cleanEvent.payload.refType = ghEvent.payload.ref_type;
+            if (cleanEvent.payload.refType === 'branch') {
+                cleanEvent.payload.branch = ghEvent.payload.ref;
+            }
         } else if (ghEvent.type === 'WatchEvent') {
             cleanEvent.payload.action = ghEvent.payload.action;
         }
 
-        cleanData[ghEvent.type].push(cleanEvent);
+        cleanData.push(cleanEvent);
     }
 });
 
