edb24dc96a49ebaf5c904aa3d1acb3e567bfeec9
Charles Sprayberry <me@cspray.io>
2021-05-30 12:42:59 -0400

    remove erroneous conditional

--- scripts/github-commits.js
+++ scripts/github-commits.js
@@ -7,30 +7,28 @@ const data = await readJSON(filename);
 const cleanData = [];
 
 data.forEach(ghEvent => {
-    if (cleanData[ghEvent.type]) {
-        const cleanEvent = {};
-        cleanEvent.id = ghEvent.id;
-        cleanEvent.type = ghEvent.type;
-        cleanEvent.createdAt = ghEvent.created_at;
-        cleanEvent.payload = {};
-        cleanEvent.payload.repoName = ghEvent.repo.name;
-        cleanEvent.payload.repoUrl = `https://github.com/${ghEvent.repo.name}`;
-        if (ghEvent.type === 'PushEvent') {
-            cleanEvent.payload.branch = ghEvent.payload.ref.replace('refs/heads/', '');
-            cleanEvent.payload.branchUrl =  `${cleanEvent.payload.repoUrl}/tree/${cleanEvent.payload.branch}`;
-            cleanEvent.payload.head = ghEvent.payload.head.substring(0, 7);
-            cleanEvent.payload.headUrl = `${cleanEvent.payload.repoUrl}/commit/${ghEvent.payload.head}`;
-        } else if (ghEvent.type === 'CreateEvent') {
-            cleanEvent.payload.refType = ghEvent.payload.ref_type;
-            if (cleanEvent.payload.refType === 'branch') {
-                cleanEvent.payload.branch = ghEvent.payload.ref;
-            }
-        } else if (ghEvent.type === 'WatchEvent') {
-            cleanEvent.payload.action = ghEvent.payload.action;
+    const cleanEvent = {};
+    cleanEvent.id = ghEvent.id;
+    cleanEvent.type = ghEvent.type;
+    cleanEvent.createdAt = ghEvent.created_at;
+    cleanEvent.payload = {};
+    cleanEvent.payload.repoName = ghEvent.repo.name;
+    cleanEvent.payload.repoUrl = `https://github.com/${ghEvent.repo.name}`;
+    if (ghEvent.type === 'PushEvent') {
+        cleanEvent.payload.branch = ghEvent.payload.ref.replace('refs/heads/', '');
+        cleanEvent.payload.branchUrl =  `${cleanEvent.payload.repoUrl}/tree/${cleanEvent.payload.branch}`;
+        cleanEvent.payload.head = ghEvent.payload.head.substring(0, 7);
+        cleanEvent.payload.headUrl = `${cleanEvent.payload.repoUrl}/commit/${ghEvent.payload.head}`;
+    } else if (ghEvent.type === 'CreateEvent') {
+        cleanEvent.payload.refType = ghEvent.payload.ref_type;
+        if (cleanEvent.payload.refType === 'branch') {
+            cleanEvent.payload.branch = ghEvent.payload.ref;
         }
-
-        cleanData.push(cleanEvent);
+    } else if (ghEvent.type === 'WatchEvent') {
+        cleanEvent.payload.action = ghEvent.payload.action;
     }
+
+    cleanData.push(cleanEvent);
 });
 
 await writeJSON('recent_github_activity.json', cleanData);
